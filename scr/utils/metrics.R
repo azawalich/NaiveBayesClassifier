31530c69ec8091ef90a113fd3294e678cbc445b2
Aleksander Zawalich <aleksander.zawalich@gmail.com>
2019-01-20 21:34:07 +0100

    deleting old metrics dir

--- scr/utils/metrics.R
+++
@@ -1,7 +0,0 @@
-# Calculates accuracy for the given set of predicted labels and ground truth labels.
-accuracyScore <- function(predLabels, trueLabels) {
-  labTable <- table(predLabels, trueLabels)
-  diagTable <- diag(nrow(labTable)) * labTable
-  
-  return(sum(diagTable) / sum(labTable))
-}
